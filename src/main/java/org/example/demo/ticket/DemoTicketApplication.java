5550e5ec210c1724c1f8d601ae4f63aacab18cd0
Kadapa <akhilsantosh.kadapa@bt.com>
2024-05-15 11:16:55 +0100

    Initial commit

---
+++ src/main/java/org/example/demo/ticket/DemoTicketApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demo/ticket/DemoTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
