9773014fcf088b5d53648e26d57ac176c51239ff
Kadapa <akhilsantosh.kadapa@bt.com>
2024-05-16 15:20:16 +0100

    Resolved  the errors in pom.xml file

--- src/main/java/org/example/demo/ticket/entity/Event.java
+++ src/main/java/org/example/demo/ticket/entity/Event.java
@@ -1,16 +1,22 @@
 package org.example.demo.ticket.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "events")
 public class Event {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String title;
     private String description;
-    }
+    private Date date;
+    private String location;
+
+
+}
+
